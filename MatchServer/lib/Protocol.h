19fd2c6b3e16bffd0341c837debbd19d3aed43f4
lunker <dongqlee@433.co.kr>
2016-11-10 11:34:12 +0900

    protocol.h 수정

--- MatchServer/lib/Protocol.h
+++ MatchServer/lib/Protocol.h
@@ -1,8 +1,47 @@
-#pragma once
+﻿#pragma once
 #include <string>
 
 using namespace std;
 
+enum SrcDstType :int
+{
+	MATCHING_SERVER = 0,
+	MATCHING_CLIENT,
+	ROOM_MANAGER,
+	PACKET_GENERATOR,
+	MONITORING_SERVER,
+	CONFIG_SERVER,
+	CONNECTION_SERVER
+};
+
+struct Header
+{
+	int length;
+	SrcDstType srcType;
+	int srcCode;
+	SrcDstType dstType;
+	int dstCode;
+	Header() {};
+	Header(int len, SrcDstType srcType, int srcCode, SrcDstType dstType, int dstCode)
+	{
+		this->length = len;
+		this->srcType = srcType;
+		this->srcCode = srcCode;
+		this->dstType = dstType;
+		this->dstCode = dstCode;
+	}
+};
+
+struct Body {
+
+	Command command;
+	string data;
+};
+
+struct Packet {
+	Header header;
+	Body body;
+};
 
 struct UserInfo
 {
@@ -15,10 +54,10 @@ enum ClientState : int {
 	Reserved
 };
 
+
 struct Policy {
 
-public: 
+public:
 	static const int MATCHING_INTERVAL = 500; // 500ms
-	static const int MATCHING_ATTEMPT = 3; // 3ȸ �õ� 
+	static const int MATCHING_ATTEMPT = 3; // 3ȸ �õ� 
 };
-
